21fb5f5b7e360445c3e760a1aba68be8e81164de
Josh <joshhazelhurst123@gmail.com>
2019-02-22 20:57:45 +1300

    Create valentinesday.java

---
+++ valentines/valentinesday.java
@@ -0,0 +1,10 @@
+public class valentinesday
+{
+  public static void main(String[] args){
+    System.out.println(",0101.0101,");
+    System.out.println("01010101010");
+    System.out.println("'010101010'");
+    System.out.println("  '01010'");
+    System.out.println("     1 ");
+    }
+ }
